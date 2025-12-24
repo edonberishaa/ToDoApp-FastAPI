b95801692aae69e3d951ffe88452fd79d0011560
Edon <edonberisha52@gmail.com>
2025-12-24 11:38:38 +0100

    database url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:edon@localhost/ToDoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "postgresql://todoapplicationdatabase_hdqe_user:MVh2f40VCJkkxVigjnCTCrDy1ZF82gLO@dpg-d55s4663jp1c73a2bdq0-a/todoapplicationdatabase_hdqe"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
